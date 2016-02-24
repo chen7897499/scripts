ad7a2842f0f4ea752537110e344f68f7bea58f05
chen7897499 <476964702@qq.com>
2016-02-24 16:06:54 +0800

    Create chinanet.js

---
+++ chinanet.js
@@ -0,0 +1,2 @@
+// chinanet bookmarklet https://wlan.ct10000.com/portal4HN/index.jsp
+javascript:document.getElementById('account').setAttribute("value","t531ww82868");document.getElementById('userPassword').setAttribute("value","112395");document.getElementById('provice').options[14].selected=true;commLogin();
